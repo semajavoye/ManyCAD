d4d4dbb99d26abb8627b97d62ad85226132f08e6
semajavoye <semaja.voye5_8@gmx.de>
2023-12-05 22:22:01 +0100

    Design

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['username'])) {
+if(!isset($_SESSION['username'])) {
     // Benutzer ist nicht eingeloggt, leite weiter zur login.php
     header("Location: login.php");
     exit();
@@ -17,8 +17,8 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leitstellen System by Semaja Voyé</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" href="css/menus.css">
     <link rel="stylesheet" href="css/var.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -34,17 +34,20 @@ if (!isset($_SESSION['username'])) {
                     <ul>
                         <li id="tab0" data-tab="user-settings">
                             <i class="fa-solid fa-user"></i>
-                            <?php if (isset($_SESSION['username'])) {
-                                echo '<span class="username">' . $_SESSION['username'] . '</span>';
+                            <?php if(isset($_SESSION['username'])) {
+                                echo '<span class="username">'.$_SESSION['username'].'</span>';
                             } ?>
                         </li>
                         <li id="logout"><button>Logout</button></li>
                         <li id="tab1" data-tab="new-call"><img src="img/desktop-icons/new-call.png" alt="">New Call</li>
                         <li id="tab2" data-tab="map-container"><img src="img/desktop-icons/maps.png" alt="">Maps</li>
-                        <li id="tab3" data-tab="searchcars"><img src="img/desktop-icons/car-search.png" alt="">Search Vehicle</li>
-                        <li id="tab4" data-tab="searchpersons"><img src="img/desktop-icons/person-search.png" alt="">Search person</li>
+                        <li id="tab3" data-tab="searchcars"><img src="img/desktop-icons/car-search.png" alt="">Search
+                            Vehicle</li>
+                        <li id="tab4" data-tab="searchpersons"><img src="img/desktop-icons/person-search.png"
+                                alt="">Search person</li>
                         <li id="tab5" data-tab="noteContainer"><img src="img/desktop-icons/notes.png" alt="">Notes</li>
-                        <li id="tab6" data-tab="active-calls"><img src="img/desktop-icons/activedispatches.png" alt="">Active Dispatches</li>
+                        <li id="tab6" data-tab="active-calls"><img src="img/desktop-icons/activedispatches.png"
+                                alt="">Active Dispatches</li>
 
                         <div class="sidebar-footer">
                             <div class="datetime">
@@ -56,11 +59,34 @@ if (!isset($_SESSION['username'])) {
                 </div>
 
                 <div class="apps-container">
+                    <div class="user-settings-main tab-content" id="user-settings">
+                        <div class="user-settings-container">
+                            <div class="title">User Settings</div>
+
+                            <div class="message"></div>
+                            <?php
+                            if(isset($_SESSION['username'])) {
+                                echo '<span class="username"> Benutzername: '.$_SESSION['username'].'</span>';
+                            }
+                            ?>
+
+                            <form action="change_password.php" method="post">
+                                <div class="new-password">
+                                    <label for="new-password" class="label">Neues Passwort:</label>
+                                    <input type="password" name="new_password" id="new-password" required>
+                                    <label for="confirm-password" class="label">Passwort bestätigen:</label>
+                                    <input type="password" name="confirm_password" id="confirm-password" required>
+                                    <button type="submit">Passwort ändern</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
                     <div class="new-call-main tab-content" id="new-call">
-                        <div class="call-infos">
-                            <div class="callform">
-                                <form id="call-form">
-                                    Notfallaufnahme
+                        <div class="new-call-container">
+                            <div class="new-call-form">
+                                <div class="title">Notfallaufnahme</div>
+                                <form id="new-call-form">
                                     <div class="form-group">
                                         <label for="call-number" class="label">Anrufsnummer:</label>
                                         <input type="number" id="call-number" name="call-number" class="input" max=6
@@ -93,41 +119,15 @@ if (!isset($_SESSION['username'])) {
                         </div>
                     </div>
 
-
-                    <div class="active-calls-main tab-content" id="active-calls">
-                        Aktive Dispatches
-                        <div class="active-calls-container">
-                            <div class="active-call-infos">
-                                <div class="active-callform">
-                                    <form id="active-call-form">
-                                        <div class="form-group">
-                                            <label for="call-number" class="label">Anrufsnummer:</label>
-                                            <span class="call-number-sp"></span>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="who" class="label">Wer?</label>
-                                            <div class="namecontainer">
-                                                <span class="firstName-sp"></span>
-                                                <span class="lastName-sp"></span>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="location" class="label">Postleitzahl:</label>
-                                            <span class="postCode-sp"></span>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                     <div class="map-main tab-content" id="map-container">
                         <div id="map"></div>
                     </div>
-                    <div class="car  tab-content" id="searchcars">
-                        <div class="car-infos">
+
+
+                    <div class="car-main tab-content" id="searchcars">
+                        <div class="car-container">
+                            <div class="title">Vehicle Infos</div>
                             <div class="topContainerHead">
-                                Vehicle Infos
                                 <form class="searchVeh">
                                     <input type="search" placeholder="Kennzeichen">
                                     <button type="submit">Search</button>
@@ -137,19 +137,19 @@ if (!isset($_SESSION['username'])) {
                                 <div class="owner">
                                     Besitzer:
                                     <div class="ownerholder">
-                                        <span class="ownersp">Semaja Voye</span>
+                                        <span class="ownersp"></span>
                                     </div>
                                 </div>
                                 <div class="plate">
                                     Kennzeichen:
                                     <div class="plateholder">
-                                        <span class="platesp">ING 365</span>
+                                        <span class="platesp"></span>
                                     </div>
                                 </div>
                                 <div class="job">
                                     Job:
                                     <div class="jobholder">
-                                        <span class="jobsp">Police</span>
+                                        <span class="jobsp"></span>
                                     </div>
                                 </div>
                             </div>
@@ -157,9 +157,9 @@ if (!isset($_SESSION['username'])) {
                     </div>
 
                     <div class="searchpersons-main tab-content" id="searchpersons">
-                        <div class="person-infos">
+                        <div class="searchpersons-container">
+                            <div class="title">Personensuche</div>
                             <div class="topContainerHead">
-                                Personensuche
                                 <div class="searchPers">
                                     <input type="search" placeholder="Vorname" id="firstName">
                                     <input type="search" placeholder="Nachname" id="lastName">
@@ -206,9 +206,9 @@ if (!isset($_SESSION['username'])) {
                     </div>
 
                     <!-- Add this form inside the noteContainer div -->
-                    <div class="noteContainer tab-content" id="noteContainer">
-                        <div class="noteField">
-                            Meine Notizen
+                    <div class="note-main tab-content" id="noteContainer">
+                        <div class="note-container">
+                            <div class="title">Meine Notizen</div>
                             <select>
                                 <option value="Notiz 1">Notiz 1</option>
                                 <option value="Notiz 2">Notiz 2</option>
@@ -224,25 +224,28 @@ if (!isset($_SESSION['username'])) {
                         </div>
                     </div>
 
-                    <div class="user-settings-main tab-content" id="user-settings">
-                        <div class="user-settings-cont">
-                            <h1>User Settings</h1>
-                            <div class="message"></div>
-                            <?php
-                            if (isset($_SESSION['username'])) {
-                                echo '<span class="username"> Benutzername: ' . $_SESSION['username'] . '</span>';
-                            }
-                            ?>
-
-                            <form action="change_password.php" method="post">
-                                <div class="new-password">
-                                    <label for="new-password" class="label">Neues Passwort:</label>
-                                    <input type="password" name="new_password" id="new-password" required>
-                                    <label for="confirm-password" class="label">Passwort bestätigen:</label>
-                                    <input type="password" name="confirm_password" id="confirm-password" required>
-                                    <button type="submit">Passwort ändern</button>
-                                </div>
-                            </form>
+                    <div class="active-calls-main tab-content" id="active-calls">
+                        <div class="title">Aktive Dispatches</div>
+                        <div class="active-calls-container">
+                            <div class="active-calls-form">
+                                <form id="active-call-form">
+                                    <div class="form-group">
+                                        <label for="call-number" class="label">Anrufsnummer:</label>
+                                        <span class="call-number-sp"></span>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="who" class="label">Wer?</label>
+                                        <div class="namecontainer">
+                                            <span class="firstName-sp"></span>
+                                            <span class="lastName-sp"></span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="location" class="label">Postleitzahl:</label>
+                                        <span class="postCode-sp"></span>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
